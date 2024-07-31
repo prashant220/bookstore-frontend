2e9b713f2484bfd8835d6e1f2977a446359df71f
prashant220 <ppoudel624@gmail.com>
2024-07-31 01:34:49 -0400

    working on code structure

---
+++ js/api.js
@@ -0,0 +1,106 @@
+export const products = {
+    product1: {
+        id: "product1",
+        title: "Cartoon Astronaut Shirt",
+        image: "../img/products/f1.jpg",
+        brand: "Addidas",
+        stars: 5,
+        price: "$90",
+        description: "Description of Product 1."
+    },
+    product2: {
+        id: "product2",
+        title: "Cool Shirt",
+        image: "../img/products/f2.jpg",
+        brand: "Addidas",
+        stars: 3,
+        price: "$70",
+        description: "Description of Product 2."
+    },
+    product3: {
+        id: "product3",
+        title: "kera",
+        image: "../img/products/f3.jpg",
+        brand: "Addidas",
+        stars: 3,
+        price: "$85",
+        description: "Description of Product 3."
+    },
+    product4: {
+        id: "product4",
+        title: "Cartoon Astronaut Shirt",
+        image: "../img/products/f4.jpg",
+        brand: "Addidas",
+        stars: 4,
+        price: "$70",
+        description: "Description of Product 4."
+    },
+    product5: {
+        id: "product5",
+        title: "Cartoon Astronaut Shirt",
+        image: "../img/products/f5.jpg",
+        brand: "Addidas",
+        stars: 3,
+        price: "$78",
+        description: "Description of Product 5."
+    },
+    product6: {
+        id: "product6",
+        title: "Cartoon Astronaut Shirt",
+        image: "../img/products/f6.jpg",
+        brand: "Addidas",
+        stars: 5,
+        price: "$50",
+        description: "Description of Product 6."
+    },
+    product7: {
+        id: "product7",
+        title: "Cartoon Astronaut Palazzo",
+        image: "../img/products/f7.jpg",
+        brand: "Addidas",
+        stars: 4,
+        price: "$90",
+        description: "Description of Product 7."
+    },
+    product8: {
+        id: "product8",
+        title: "Cartoon Astronaut Tops",
+        image: "../img/products/f8.jpg",
+        brand: "Addidas",
+        stars: 5,
+        price: "$99",
+        description: "Description of Product 8."
+    }
+};
+//new
+export const new_books = {
+    product9: {
+        id: "product9",
+        title: "book1",
+        image: "../img/products/f1.jpg",
+        brand: "Addidas",
+        stars: 5,
+        price: "$90",
+        description: "Description of Product 1."
+    },
+    product10: {
+        id: "product10",
+        title: "book2",
+        image: "../img/products/f2.jpg",
+        brand: "Addidas",
+        stars: 3,
+        price: "$70",
+        description: "Description of Product 2."
+    },
+    product11: {
+        id: "product11",
+        title: "book3",
+        image: "../img/products/f3.jpg",
+        brand: "Addidas",
+        stars: 3,
+        price: "$85",
+        description: "Description of Product 3."
+    },
+  
+ 
+};
